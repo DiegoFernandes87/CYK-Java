7ed72eddc59b0b1a149095672ad9a75be22c0e93
Akshay Hegde <akshay.hegde17@gmail.com>
2013-03-14 20:19:34 -0700

    First draft commit

--- src/Cyk.java
+++ src/Cyk.java
@@ -1,14 +1,13 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.util.Scanner;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
 /**
- * Implements the CYK Algorithm that uses a context-free grammar file and
- * checks to see whether a given string is accepted by that grammar.
- * CS154 Section 1
+ * Implements the CYK Algorithm that uses a context-free grammar file and checks
+ * to see whether a given string is accepted by that grammar. CS154 Section 1
  */
 public class Cyk
 {
@@ -19,32 +18,36 @@ public class Cyk
    public static final int PARSE_ERROR = 2;
    public static final int TOTAL_ARGS = 2;
 
-   /* The 2 dimensional table for the CYK algorithm */ 
-   private static ArrayList < String > [][] table;
+   /* The 2 dimensional table for the CYK algorithm */
+   private static ArrayList<String>[][] table;
 
-   /* The list of productions given in the grammar file 
-    * The only productions stored in the HashSet are productions in the 
-    * form A -> BC where A, B, and C are Variables
+   /*
+    * The list of productions given in the grammar file The only productions
+    * stored in the HashSet are productions in the form A -> BC where A, B, and
+    * C are Variables
     */
-   private static HashMap < String, String[] > productions;
-   private static HashMap < String, String[] > variables;
-   private static HashMap < String, Character > terminals;
+   private static HashMap<String, String[]> productions;
+   private static HashMap<String, String[]> variables;
+   private static HashMap<String, Character> terminals;
 
    /* The start variable */
    private static String startVariable;
 
    /**
     * Processes the grammar file and builds the list of productions
-    * @param fin the BufferedReader object that contains the grammar file
-    * @return true if parsing was successful, false otherwise
+    * @param file the string representing the path of the grammar file
     */
-   public static boolean processGrammarFile(BufferedReader fin)
+   public static void processGrammarFile(String file) 
    {
-      try
+      File grammarFile = null;
+      Scanner in = null;
+      try 
       {
-         String[] line = fin.readLine().split(":");
+         grammarFile = new File(file);
+         in = new Scanner(grammarFile);
+         String[] line = in.nextLine().split(":");
          startVariable = line[0];
-         while (!line[0].isEmpty())
+         while (!in.hasNextLine())
          {
             String variable = line[0];
             String[] buffer = new String[1];
@@ -57,81 +60,87 @@ public class Cyk
             else
             {
                String[] rest = line[1].split(",");
-               variables.put(variable, rest);
-               productions.put(variable, rest);
-               return true;
+               if (rest != null)
+               {
+                  variables.put(variable, rest);
+                  productions.put(variable, rest);
+               }
             }
-            fin.readLine().split(":");
          }
       }
-      catch  (IOException ex)
+      catch (IOException ex) 
       {
-         return false;
+         ex.printStackTrace();
       }
-      return false;
    }
 
    /**
-    * Tests the string against the given grammar file using the 
-    * CYK Algorithm. 
+    * Tests the string against the given grammar file using the CYK Algorithm.
     * @param w the input string to test
     * @return true if string w is accepted by the grammar, false otherwise.
     */
    public static boolean processString(String w)
    {
-      // Initialize the table
-      table = new ArrayList [w.length()][];
-      for (int i = 0; i < w.length(); ++i)
+      int length = w.length();
+      table = new ArrayList[length][];
+      for (int i = 0; i < length; ++i)
       {
-         table[i] = new ArrayList[w.length()];
-         for (int j = 0; j < w.length(); ++j)
-         {
-            table[j] = new ArrayList(); 
-         }
+         table[i] = new ArrayList[length];
+         for (int j = 0; j < length; ++j)
+            table[i][j] = new ArrayList < String > ();
       }
-      
-      // Start CYK Algorithm
-      for (int i = 0; i < w.length(); ++i)
+      // Start CYK algorithm
+      for (int i = 0; i < length; ++i)
       {
-         // Get the list of variables
-         Set<String> keys = variables.keySet();
-         for (String key: keys)
-         {
+         Set<String> keys = terminals.keySet();
+         for (String key : keys)
             if (terminals.get(key).charValue() == w.charAt(i))
                table[i][i].add(key);
+
+      }
+      for (int l = 2; l <= length; ++l)
+      {
+         for (int i = 0; i <= length - l; ++i)
+         {
+            int j = i + l - 1;
+            for (int k = i; k <= j - 1; ++k)
+            {
+               Set<String> keys = variables.keySet();
+               for (String key : keys)
+               {
+                  String[] values = variables.get(key);
+                  if (table[i][k].contains((values[0]))
+                        && table[k + 1][j].contains(values[1]))
+                     table[i][j].add(key);
+               }
+            }
          }
       }
+      if (table[0][length].contains(startVariable))
+         return true;
       return false;
    }
 
    /**
     * Takes a given grammar file as the input and a given string to test
-    * against that grammar
-    * @param args the given command-line arguments. They will consist of
-    *             the grammar file and the string to test, strictly in that
-    *             order.
+    * against that grammar.
+    * @param args the given command-line arguments. They will consist of the
+    *            grammar file and the string to test, strictly in that order.
     */
    public static void main(String[] args)
    {
-      BufferedReader br = null;
+      productions = new HashMap<String, String[]>();
+      variables = new HashMap<String, String[]>();
+      terminals = new HashMap<String, Character>();
       if (args.length != TOTAL_ARGS)
       {
          System.out.println("Usage: java Cyk grammar_file some_string");
          System.exit(FILE_ERROR);
       }
-      try
-      {
-         br = new BufferedReader(new FileReader(args[GRAMMAR_FILE]));
-         if (!processGrammarFile(br))
-         {
-            System.out.println("Grammar file could not be parsed.");
-            System.exit(PARSE_ERROR);
-         }
-         System.out.println(processString(args[LAST_ARG]));
-      }
-      catch (IOException ex)
-      {
-         System.out.println("File IO Error: " + ex);
-      }
+      processGrammarFile(args[GRAMMAR_FILE]);
+      if (processString(args[LAST_ARG]))
+         System.out.println("true");
+      else
+         System.out.println("false");
    }
 }
